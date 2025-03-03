63e7166cfcfbb8e9fc9247d196ba4545b21ee8f8
manaspohane2307 <manaspohane@gmail.com>
2025-03-04 00:12:50 +0530

    Time: 1 ms (100%), Space: 42.4 MB (80.79%) - LeetHub

---
+++ 3447-clear-digits/3447-clear-digits.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String clearDigits(String s) {
+
+        StringBuilder ans = new StringBuilder();
+
+        for (int i = 0; i < s.length(); i++) {
+            if (Character.isDigit(s.charAt(i))) {
+                if (ans.length() > 0) {
+                    ans.deleteCharAt(ans.length() - 1);
+                }
+            } else {
+                ans.append(s.charAt(i));
+            }
+        }
+
+        return ans.toString();
+    }
+}
