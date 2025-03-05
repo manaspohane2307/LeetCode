74c17155d322b732bca438e56c9f16247fe51a05
Manas Pohane <manaspohane@gmail.com>
2025-03-05 18:58:18 +0530

    Time: 0 ms (100%), Space: 40.8 MB (16.39%) - LeetHub

--- 2649-count-total-number-of-colored-cells/2649-count-total-number-of-colored-cells.java
+++ 2649-count-total-number-of-colored-cells/2649-count-total-number-of-colored-cells.java
@@ -1,10 +1,10 @@
 class Solution {
     public long coloredCells(int n) {
-        long res=1;
-        for(int i=0;i<n;i++){
-            res += 4*i;
-        }
-        return res;
-        // return 1 + 4*(n*(n-1)/2);
+        // long res=1;
+        // for(int i=0;i<n;i++){
+        //     res += 4*i;
+        // }
+        // return res;
+        return 1 + (long)2*n*(n-1);
     }
 }
