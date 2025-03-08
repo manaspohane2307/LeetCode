b61e87c068feb0b98375b52372a91ba9bf9ce1ae
Manas Pohane <manaspohane@gmail.com>
2025-03-08 11:19:14 +0530

    Time: 1 ms (92.1%), Space: 41.3 MB (86.78%) - LeetHub

--- 2463-minimum-recolors-to-get-k-consecutive-black-blocks/2463-minimum-recolors-to-get-k-consecutive-black-blocks.java
+++ 2463-minimum-recolors-to-get-k-consecutive-black-blocks/2463-minimum-recolors-to-get-k-consecutive-black-blocks.java
@@ -1,18 +1,18 @@
 class Solution {
     public int minimumRecolors(String blocks, int k) {
-        int l=0;
-        int recolor=0;
-        int res=k;
-        for(int r=0;r<blocks.length();r++){
+        int l=0;//left pointer of sliding window
+        int recolor=0;//no of white blocks in current window
+        int res=k;//worst case - all blocks colored white
+        for(int r=0;r<blocks.length();r++){//r is right pointer
             if(blocks.charAt(r) == 'W'){
                 recolor += 1;
             }
-            if((r-l+1) == k){
-                res = Math.min(res, recolor);
+            if((r-l+1) == k){//when window size reaches k, update result and slide window
+                res = Math.min(res, recolor); //update result
                 if(blocks.charAt(l) == 'W'){
                     recolor -= 1;
                 }
-                l += 1;
+                l += 1;//slide the window forward
             }
         }
         return res;
